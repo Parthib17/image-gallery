a0e3e5a911692fd21810391cd098fc0b3d3d0371
Eahtasham <eahtashamummam@gmail.com>
2025-05-31 21:11:53 +0530

    fixes

--- image_gallery-old.sql
+++ image_gallery-old.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 31, 2025 at 05:41 PM
+-- Generation Time: May 31, 2025 at 12:43 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -68,6 +68,7 @@ INSERT INTO `images` (`id`, `file_name`, `title`, `description`, `uploaded_at`) 
 (33, '683ac67fbf5cd_hills.jpg', 'mountain', 'jkgfhfghhg', '2025-05-31 09:06:07'),
 (34, '683ac6975e911_stair.jpg', 'siri', 'dfgsfdsf', '2025-05-31 09:06:31'),
 (35, '683ac6a68779e_gully.jpg', 'western gully', 'sefghkyujhf', '2025-05-31 09:06:46'),
+(36, '683ac6b8215e8_couple.jpg', 'Mongolian couple', 'edfgsfg hhg', '2025-05-31 09:07:04'),
 (37, '683ac6ca4f41e_market.jpg', 'Japanese market ', 'egeefed wsd', '2025-05-31 09:07:22'),
 (39, '683ac6e731766_Home.jpg', 'houses', 'sdfghdsf ', '2025-05-31 09:07:51'),
 (40, '683ada2c8a59f_small bird.jpg', 'Small and cute bird', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, ', '2025-05-31 10:30:04'),
@@ -81,11 +82,7 @@ INSERT INTO `images` (`id`, `file_name`, `title`, `description`, `uploaded_at`) 
 (48, '683adaf3d2832_forest-9504070_1920.jpg', 'Beautiful Forest', 'scovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good', '2025-05-31 10:33:23'),
 (49, '683adb0a256ff_animals-9533774_1920.jpg', 'Dog', 'in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin wor', '2025-05-31 10:33:46'),
 (50, '683adb208405a_istockphoto-1290233518-1024x1024.jpg', 'Cat\'s face', 'scovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good', '2025-05-31 10:34:08'),
-(51, '683adb324ae7c_white-cat-9595396_1920.jpg', 'White cat', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, ', '2025-05-31 10:34:26'),
-(54, '683b17db6203a_positive-thinking.gif', 'Chenab River', 'It is an indian river.', '2025-05-31 14:53:15'),
-(55, '683b180627bc7_code.png', 'dgfgd', 'dgdgf', '2025-05-31 14:53:58'),
-(57, '683b1ccc5759d_positive-thinking.gif', 'Happy Sad', 'Happt', '2025-05-31 15:14:20'),
-(58, '683b22a22ca6f_logo.png', 'logo', 'logooooooooo', '2025-05-31 15:39:14');
+(51, '683adb324ae7c_white-cat-9595396_1920.jpg', 'White cat', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, ', '2025-05-31 10:34:26');
 
 -- --------------------------------------------------------
 
@@ -109,10 +106,7 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
 (2, 'parthib', 'parthib@hotmail.com', 'parthib123'),
 (3, 'deb', 'deb123@gmail.com', 'deb123'),
 (6, 'souvik', 'souvik@hotmail.com', 'souvik123'),
-(7, 'Sourav Samanta', 'sourav123@gmail.com', '123'),
-(8, 'Eahtasham', 'eahtasham123@gmail.com', 'eu123'),
-(9, 'newuser', 'newuser123@gmail.com', 'newuser123'),
-(10, 'Eahtasham1', 'Eahtasham1@gmail.com', 'eu1');
+(7, 'Sourav Samanta', 'sourav123@gmail.com', '123');
 
 --
 -- Indexes for dumped tables
@@ -153,13 +147,13 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
